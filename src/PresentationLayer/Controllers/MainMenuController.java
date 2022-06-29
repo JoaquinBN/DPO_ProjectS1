110f0dfe86a13c164db5e57cc2517e9727368329
albert-mr <58224660+albert-mr@users.noreply.github.com>
2022-06-30 00:42:01 +0200

    main menu view and controller fixed

--- src/PresentationLayer/Controllers/MainMenuController.java
+++ src/PresentationLayer/Controllers/MainMenuController.java
@@ -4,5 +4,31 @@ import PresentationLayer.Views.MainMenuView;
 
 public class MainMenuController {
     private MainMenuView mainMenuView;
+    private ComposerController composerController;
+    private ConductorController conductorController;
+
+    public MainMenuController(MainMenuView mainMenuView, ComposerController composerController, ConductorController conductorController) {
+        this.mainMenuView = new MainMenuView();
+        this.composerController = composerController;
+        this.conductorController = conductorController;
+    }
+
+    public void mainMenuDisplay() {
+        switch (mainMenuView.mainMenuDisplay()) {
+            case 'A':
+                composerController.managementMode();
+                break;
+            case 'B':
+                //conductorController.conductorMenu();
+                break;
+            default:
+                mainMenuView.showError("\nInvalid role. Choose A or B.\n");
+                mainMenuDisplay();
+                break;
+        }
+
+
+    }
+
 
 }
--- src/PresentationLayer/Views/MainMenuView.java
+++ src/PresentationLayer/Views/MainMenuView.java
@@ -4,25 +4,28 @@ import java.util.Scanner;
 
 public class MainMenuView {
 
-    private char mainMenuDisplay(){
+    public char mainMenuDisplay(){
         Scanner input = new Scanner(System.in);
         char option;
         System.out.println("""
-             _____ _            _____      _       _
-            /__   \\ |__   ___  /__   \\_ __(_) __ _| |___
-              / /\\/ '_ \\ / _ \\   / /\\/ '__| |/ _` | / __|
-             / /  | | | |  __/  / /  | |  | | (_| | \\__ \\
-             \\/   |_| |_|\\___|  \\/   |_|  |_|\\__,_|_|___/
-            
-            Welcome to The Trials. Who are you?
-            
-                A) The Composer
-                B) This year’s Conductor
-               
-            Enter a role: 
-            """);
+                 _____ _            _____      _       _
+                /__   \\ |__   ___  /__   \\_ __(_) __ _| |___
+                  / /\\/ '_ \\ / _ \\   / /\\/ '__| |/ _` | / __|
+                 / /  | | | |  __/  / /  | |  | | (_| | \\__ \\
+                 \\/   |_| |_|\\___|  \\/   |_|  |_|\\__,_|_|___/
+                
+                Welcome to The Trials. Who are you?
+                
+                    A) The Composer
+                    B) This year’s Conductor
+                """);
+        System.out.print("Enter a role: ");
         option = input.next().charAt(0);
 
         return option;
+    }
+
+    public void showError(String error) {
+        System.out.println(error);
     }
 }
