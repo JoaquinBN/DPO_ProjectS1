af95ea388355b406b945cdd12a7e7acdbc2b5817
albert-mr <58224660+albert-mr@users.noreply.github.com>
2022-06-29 05:02:31 +0200

    s

--- src/PersistanceLayer/EditionFileManager.java
+++ src/PersistanceLayer/EditionFileManager.java
@@ -15,24 +15,21 @@ public class EditionFileManager {
     }
 
     public void writeEditions(ArrayList<Edition> Editions) throws IOException {
+
         CSVWriter writer = new CSVWriter(new FileWriter("Files/Editions.csv", true),
                 CSVWriter.DEFAULT_SEPARATOR,
                 CSVWriter.NO_QUOTE_CHARACTER, CSVWriter.NO_ESCAPE_CHARACTER, "\n");
+
+
+
         for (Edition edition : Editions) {
-            String[] data = {String.valueOf(edition.getYear()), String.valueOf(edition.getNumberOfPlayers()), Arrays.toString(edition.getTrials())};
+            String[] data = {String.valueOf(edition.getYear()), String.valueOf(edition.getNumberOfPlayers()), String.valueOf(edition.getNumberOfTrials())};
             writer.writeNext(data);
         }
         writer.close();
     }
 
-    public void readEditions() throws IOException, CsvException {
-        CSVReader reader = new CSVReader(new FileReader("csv/Editions.csv"));
-        List<String[]> editions = reader.readAll();
-        for (String[] edition : editions) {
-            Edition e = new Edition(Integer.parseInt(edition[0]), Integer.parseInt(edition[1]), Arrays.asList(edition[2].split(",")));
-            Edition.Editions.add(e);
-        }
-    }
+
 
 
 }
